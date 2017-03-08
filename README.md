5a74e35b0dd6caa5b8f6c30cc0ffe1074d38530a
zlalvani <zlalvani@gmail.com>
2017-03-07 21:24:36 -0500

    initial hello world flask server with nginx and docker-compose

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# reciprep-api
-API for reciprep app
+# reciprep-dev
+
+This is the repository for all back-end services for ReciPrep.
+
+For development, use docker-compose to start an environment on your local machine.
--- api/app.py
+++ api/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/api')
 def hello_world():
     return 'Hello World ReciPrep'
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
+    # app.run(debug=True,host='0.0.0.0')
+    app.run(debug=True)
